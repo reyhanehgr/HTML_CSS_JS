0e4d1194f0c0342b83ad3e7f6b08cff06a898228
reyhaneh gerami <32893822+reyhanehgr@users.noreply.github.com>
2025-10-21 16:05:19 +0300

    Add files via upload

---
+++ JS Button Message/javascript.js
@@ -0,0 +1,7 @@
+
+function showMessage(){
+  const Message="Hello! This MESSAGE was displayed using JavaScript."
+  alert(Message);
+  document.getElementById("jsOutput").innerHTML=Message;
+  console.log(Message);
+}
