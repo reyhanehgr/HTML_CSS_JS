6b98dc5e4641ede6c5c4c5789bde11fa8586928f
reyhaneh gerami <32893822+reyhanehgr@users.noreply.github.com>
2025-10-21 16:05:45 +0300

    Delete JS Button Message directory

--- JS Button Message/javascript.js
+++
@@ -1,7 +0,0 @@
-
-function showMessage(){
-  const Message="Hello! This MESSAGE was displayed using JavaScript."
-  alert(Message);
-  document.getElementById("jsOutput").innerHTML=Message;
-  console.log(Message);
-}
